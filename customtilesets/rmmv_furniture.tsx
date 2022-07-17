4e8dbf01eeee1e214ab7b9fee73b109ceba98a67
clam0r <clamor@outlook.de>
2022-07-17 13:42:57 +0200

    added buildings with labels

---
+++ customtilesets/rmmv_furniture.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="rmmv_furniture" tilewidth="32" tileheight="32" tilecount="576" columns="24">
+ <image source="rmmv_furniture___rtp_edits_by_starbirdresources_dboquop.png" width="768" height="768"/>
+</tileset>
